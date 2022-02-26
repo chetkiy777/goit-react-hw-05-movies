a1f31d6c44525410f886dcf3f6fc867e40cf5cd5
Igor Gladkiy <egunka1988@gmail.com>
2022-02-26 13:44:06 +0200

    add styles

--- src/components/pages/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage.js
@@ -1,12 +1,14 @@
+import style from './styles.module.css';
 import { useEffect, useState } from 'react';
-import { Routes, Route, useParams } from 'react-router-dom';
+import { Routes, Route, useParams, Link, useMatch } from 'react-router-dom';
 import { ApiService } from 'API/apiService';
 import Cast from './Cast';
 import Reviews from './Reviews';
+import Container from 'components/Container/Container';
 const apiService = new ApiService();
 
 const MovieDetailsPage = () => {
-  const [filmInfo, setFilmInfo] = useState();
+  const [filmInfo, setFilmInfo] = useState(null);
 
   const { movieId } = useParams();
 
@@ -15,14 +17,52 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   return (
-    <div>
+    <Container>
       {filmInfo && (
-        <div>
-          <h1>{filmInfo.original_title}</h1>
-          <p>Overview: {filmInfo.overview}</p>
+        <div className={style.filmInfo}>
+          <div className={style.thumb}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${filmInfo.poster_path}`}
+              alt=""
+            />
+          </div>
+
+          <div className={style.aboutFilm}>
+            <h2>{filmInfo.original_title}</h2>
+            <p>User score: 74%</p>
+            <p className={style.overview}>
+              <p>
+                <b>Overview: </b>
+              </p>
+              <p>{filmInfo.overview}</p>
+            </p>
+
+            <p>
+              <p>
+                <b>Genres:</b>
+              </p>
+              Action
+            </p>
+          </div>
         </div>
       )}
-    </div>
+      <div className={style.additionalInfo}>
+        <span>Additional Info</span>
+
+        <ul>
+          <li className={style.castLink}>
+            <Link exact="true" to={`/credits`}>
+              Cast
+            </Link>
+          </li>
+          <li>
+            <Link exact="true" to={`/reviews`}>
+              Review
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Container>
   );
 };
 
