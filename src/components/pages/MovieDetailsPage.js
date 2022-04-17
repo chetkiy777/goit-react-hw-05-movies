873a98d59afd37fbf801541982076bcd33ad032a
Igor Gladkiy <egunka1988@gmail.com>
2022-04-17 23:07:29 +0300

    styled MoviesDetailsPage

--- src/components/pages/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage.js
@@ -2,7 +2,7 @@ import style from './styles.module.css';
 import { useEffect, useState } from 'react';
 import { useParams, Link, Outlet } from 'react-router-dom';
 import { ApiService } from 'API/apiService';
-import { Container } from 'components/App.styled';
+import { Container, MainInfoBlock, AdditionalInfoBlock, AboutFilmInfo } from 'components/pages/Pages.styled';
 
 const apiService = new ApiService();
 
@@ -15,29 +15,33 @@ const MovieDetailsPage = ({setReviews, setActors}) => {
     apiService.getFilmDetails(Number(movieId)).then(setFilmInfo);
   }, [movieId])
 
-    useEffect(() => {
-    apiService.getFilmActors(Number(movieId)).then(setActors);
-  }, [movieId, setActors])
+  useEffect(() => {
+  apiService.getFilmActors(Number(movieId)).then(setActors);
+}, [movieId, setActors])
 
-    useEffect(() => {
-    apiService.getFilmReviews(Number(movieId)).then(setReviews);
-  }, [movieId, setReviews])
+  useEffect(() => {
+  apiService.getFilmReviews(Number(movieId)).then(setReviews);
+}, [movieId, setReviews])
 
   return (
     <Container>
       {filmInfo && (
-        <div className={style.filmInfo}>
-          <div className={style.thumb}>
+      <div>
+
+        <MainInfoBlock>
+          <div>
             <img
               src={`https://image.tmdb.org/t/p/w500/${filmInfo.poster_path}`}
               alt=""
             />
           </div>
 
-          <div className={style.aboutFilm}>
+          <AboutFilmInfo>
             <h2>{filmInfo.original_title}</h2>
+
             <p>User score: 74%</p>
-            <div className={style.overview}>
+
+            <div>
               <p>
                 <b>Overview: </b>
               </p>
@@ -48,25 +52,26 @@ const MovieDetailsPage = ({setReviews, setActors}) => {
               <p><b>Genres:</b></p>
               {filmInfo.genres.map(genre => <span key={genre.id}> {genre.name} </span>)}
             </div>
+          </AboutFilmInfo>
+        </MainInfoBlock>
 
-            <div className={style.additionalInfo}>
-              <span>Additional Info</span>
+        <AdditionalInfoBlock>
+          <span>Additional Info</span>
 
-              <ul>
-                <li className={style.castLink}>
-                  <Link to="cast">Cast</Link>
-                </li>
-                <li>
-                  <Link to="reviews">Reviews</Link>
-                </li>
-              </ul>
+          <ul>
+            <li>
+              <Link to="cast">Cast</Link>
+            </li>
+            <li>
+              <Link to="reviews">Reviews</Link>
+            </li>
+          </ul>
 
-            </div>
+        </AdditionalInfoBlock>
 
-            <Outlet/>
-
-          </div>
-        </div>          
+        <Outlet/>
+          
+      </div>          
       )}
     </Container>
   );
